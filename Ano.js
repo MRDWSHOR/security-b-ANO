3ea7cc9bcf0f42f8229f50b36cc03aecdd4ce571
MRDWSHOR <95427109+MRDWSHOR@users.noreply.github.com>
2021-12-03 23:04:22 +0300

    anas

--- Ano.js
+++ Ano.js
@@ -21,7 +21,7 @@ var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
 const prefix = "a!";
-client.login("OTE2NDE3MDg1Njc1MDI0NDY1.Yap16w.OnfPgRdtXyhHM5u0M3ShK8yYDA4");
+client.login("OTE2NDE3MDg1Njc1MDI0NDY1.Yap16w.1c_AWSVDcKkzsf0AoIL3IcYrtUE");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
